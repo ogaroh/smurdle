28283350b5d87e0c82eb427cabad73e947649b99
ogaroh <ogaroerick1998@gmail.com>
2024-03-01 16:14:19 +0300

    feat: android launcher icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
-import 'package:path_provider/path_provider.dart';
 import "app/app.dart";
 
-import 'smurdle/data/word_list.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // get dictionary
-  final Directory dir = await getApplicationDocumentsDirectory();
-  final String path = dir.path;
-  await rootBundle.loadString("$path/test.txt");
 
   return runApp(const App());
 }
